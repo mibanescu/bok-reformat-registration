23938f8ecef464edac92f2f6ab6354e3635f00f5
Mihai Ibanescu <mihai.ibanescu@gmail.com>
2019-05-29 22:56:26 -0400

    Added tox config, moved tests around

---
+++ tests/functional/test_root.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class FunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from registrationcsv import main
+        app = main({})
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def test_root(self):
+        res = self.testapp.get('/', status=200)
+        self.assertTrue(b'Pyramid' in res.body)
