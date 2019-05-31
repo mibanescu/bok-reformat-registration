6a4590e6c03f8ee679f419bca1da778d6dec3891
Mihai Ibanescu <mihai.ibanescu@gmail.com>
2019-05-30 21:37:26 -0400

    Simple request-response

--- registrationcsv/views/default.py
+++ registrationcsv/views/default.py
@@ -1,6 +1,22 @@
+from pyramid.response import Response
 from pyramid.view import view_config
+import logging
+
+log = logging.getLogger(__name__)
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
+@view_config(route_name='home', renderer='../templates/home.jinja2')
+def home(request):
     return {'project': 'registrationcsv'}
+
+
+@view_config(route_name='process', renderer='')
+def process(request):
+    fstg = request.POST['csv_file']
+    data = fstg.file.read()
+    return Response(
+        content_type="text/csv",
+        content_disposition="attachment; filename=bar.csv",
+        content_encoding="binary",
+        content_length=len(data),
+        body=data)
